6a231c2db859768f8dbc832c8019ba591d71efa7
Jonathan Munoz <madzumo@gmail.com>
2024-12-30 12:32:54 -0500

    add initial code for menu structure

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	headerMenu = `
+
++========================================================+
+|    _              _               ____     ___   __  __|
+|   / \     _   _  | |_    ___     | __ )   / _ \  \ \/ /|
+|  / _ \   | | | | | __|  / _ \    |  _ \  | | | |  \  / |
+| / ___ \  | |_| | | |_  | (_) |   | |_) | | |_| |  /  \ |
+|/_/   \_\  \__,_|  \__|  \___/    |____/   \___/  /_/\_\|
++========================================================+
+
+`
+	lipHeaderStyle   = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("46"))
+	lipManifestStyle = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("56"))
+)
+
+func main() {
+	fmt.Println("hello world")
+	ShowMenuList(lipHeaderStyle.Render(headerMenu) + "\n" + lipManifestStyle.Render(getManifest()))
+}
+
+func getManifest() string {
+
+}
---
+++ cmd/menulist.go
@@ -0,0 +1,343 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/atotto/clipboard"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const listHeight = 14
+
+var (
+	titleStyle          = lipgloss.NewStyle().MarginLeft(2).Foreground(lipgloss.Color("111"))
+	itemStyle           = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle   = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle     = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle           = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	menuMainColor       = "205"
+	menuSettingsColor   = "111"
+	menuSMTPcolor       = "184"
+	textPromptColor     = "141" //"100" //nice: 141
+	textInputColor      = "193" //"40" //nice: 193
+	textErrorColorBack  = "1"
+	textErrorColorFront = "15"
+	textResultJob       = "141" //PINK"205"
+	textJobOutcomeFront = "216"
+	// txtJobOutcomeBack   = "205"
+
+	menuTOP = []string{
+		"Toggle Provider",
+		"Change Number of Boxes to deploy",
+		"Enter Linode API Token",
+		"Enter AWS Key",
+		"Enter AWS Secret",
+		"Run Deployment",
+		"Save Settings",
+	}
+	// menuSettings = [][]string{ //menu Text + prompt text
+	// 	{"Set Iperf Server IP", "Enter Iperf Server IP:"},
+	// 	{"Set Iperf Port number", "Enter Iperf Port Number:"},
+	// 	{"Configure Email Settings", ""},
+	// 	{"Toggle CloudFlare Test", ""},
+	// 	{"Toggle M-Labs Test", ""},
+	// 	{"Toggle Speedtest.net", ""},
+	// 	{"Toggle Show Browser on Speed Tests", ""},
+	// 	{"Set Repeat Test Interval", "Enter Repeat Interval in Seconds:"},
+	// 	{"Set MSS Size", "Enter Maximum Segment Size (MSS) for Iperf Test:"},
+	// 	{"Set Iperf Retry Timeout", "Enter Max seconds Iperf will Retry before cancelling:"},
+	// }
+)
+
+type MenuState int
+
+const (
+	StateMainMenu MenuState = iota
+	StateSettingsMenu
+	StateResultDisplay
+	StateSpinner
+	StateTextInput
+)
+
+type backgroundJobMsg struct {
+	result string
+}
+
+type continueJobs struct {
+	jobResult  string
+	iperfError bool
+}
+
+type JobList int
+
+type MenuList struct {
+	list                list.Model
+	choice              string
+	header              string
+	headerIP            string
+	state               MenuState
+	prevState           MenuState
+	prevMenuState       MenuState
+	spinner             spinner.Model
+	spinnerMsg          string
+	backgroundJobResult string
+	textInput           textinput.Model
+	inputPrompt         string
+	textInputError      bool
+	manifestProvider    string
+	manifestBoxes       int
+	manifestLinodeAPI   string
+	manifestAWSkey      string
+	manifestAWSsecret   string
+}
+
+func (m MenuList) Init() tea.Cmd {
+	return nil
+}
+
+func (m MenuList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch m.state {
+	case StateMainMenu:
+		return m.updateMainMenu(msg)
+	case StateSpinner:
+		return m.updateSpinner(msg)
+	case StateTextInput:
+		return m.updateTextInput(msg)
+	default:
+		return m, nil
+	}
+}
+
+func (m *MenuList) updateMainMenu(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.MouseMsg:
+		if msg.Action == tea.MouseActionPress && msg.Button == tea.MouseButtonLeft {
+			err := clipboard.WriteAll(m.headerIP)
+			if err != nil {
+				fmt.Println("Failed to copy to clipboard:", err)
+			}
+		}
+		return m, nil
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = string(i)
+				switch m.choice {
+				case menuTOP[0], menuTOP[1], menuTOP[2], menuTOP[4]:
+					m.prevState = m.state
+					m.prevMenuState = m.state
+					m.state = StateSpinner
+					return m, tea.Batch(m.spinner.Tick, m.startBackgroundJob())
+				case menuTOP[3]:
+					m.prevState = m.state
+					m.list.Title = "Main Menu->Settings"
+					m.header = "Pepa Header"
+					selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color(menuSettingsColor))
+					m.list.Styles.Title = lipgloss.NewStyle().MarginLeft(2).Foreground(lipgloss.Color(menuSettingsColor))
+					m.state = StateSettingsMenu
+					m.updateListItems()
+					return m, nil
+				}
+			}
+			return m, nil
+		}
+		// case jobListMsg:
+
+		// 	// m.state = StateResultDisplay
+		// 	// return m, nil
+		// 	m.prevState = m.state
+		// 	m.state = StateSpinner
+		// 	return m, tea.Batch(m.spinner.Tick, m.startBackgroundJob())
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m *MenuList) updateTextInput(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.textInput, cmd = m.textInput.Update(msg)
+	m.textInputError = false
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter:
+			m.inputPrompt = m.textInput.Value() // User pressed enter, save the input
+
+			switch m.inputPrompt {
+			case m.inputPrompt:
+				fmt.Println("yep")
+			}
+			m.prevState = m.state
+			m.state = StateResultDisplay
+			return m, nil
+		case tea.KeyEsc:
+			// m.state = StateSettingsMenu
+			m.state = m.prevState
+			return m, nil
+		}
+	}
+
+	return m, cmd
+}
+
+func (m *MenuList) updateSpinner(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		// case "q", "esc":
+		// 	m.backgroundJobResult = "Job Cancelled"
+		// 	m.state = StateResultDisplay
+		// 	return m, nil
+		default:
+			// For other key presses, update the spinner
+			var cmd tea.Cmd
+			m.spinner, cmd = m.spinner.Update(msg)
+			return m, cmd
+		}
+	case backgroundJobMsg:
+		return m, tea.Batch(m.spinner.Tick, m.startBackgroundJob())
+	case continueJobs:
+		return m, tea.Batch(m.spinner.Tick, m.startBackgroundJob())
+	default:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+}
+
+func (m MenuList) View() string {
+	switch m.state {
+	case StateMainMenu, StateSettingsMenu:
+		return m.header + "\n" + m.list.View()
+	case StateSpinner:
+		return m.viewSpinner()
+	case StateTextInput:
+		return m.viewTextInput()
+	default:
+		return "Unknown state"
+	}
+}
+
+func (m MenuList) viewSpinner() string {
+	// tea.ClearScreen()
+	spinnerBase := fmt.Sprintf("\n\n   %s %s\n\n", m.spinner.View(), m.spinnerMsg)
+
+	// return spinnerBase + m.jobOutcome
+	return spinnerBase + lipgloss.NewStyle().Foreground(lipgloss.Color(textJobOutcomeFront)).Bold(true).Render(m.jobOutcome)
+}
+
+func (m MenuList) viewTextInput() string {
+	promptStyle := lipgloss.NewStyle().Foreground(lipgloss.Color(textPromptColor)).Bold(true)
+	return fmt.Sprintf("\n\n%s\n\n%s", promptStyle.Render(m.inputPrompt), m.textInput.View())
+
+}
+
+func (m *MenuList) updateListItems() {
+	switch m.state {
+	case StateMainMenu:
+		items := []list.Item{}
+		for _, value := range menuTOP {
+			items = append(items, item(value))
+		}
+		m.list.SetItems(items)
+		// case StateSettingsMenu:
+		// 	items := []list.Item{}
+		// 	for _, value := range menuSettings {
+		// 		items = append(items, item(value[0]))
+		// 	}
+		// 	m.list.SetItems(items)
+	}
+
+	m.list.ResetSelected()
+}
+
+func (m *MenuList) startBackgroundJob() tea.Cmd {
+	fmt.Println("started job")
+
+	return func() tea.Msg {
+		return backgroundJobMsg{result: "Job Complete!"}
+	}
+
+}
+
+func ShowMenuList(header string) {
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color(menuMainColor))
+	titleStyle = lipgloss.NewStyle().MarginLeft(2).Foreground(lipgloss.Color(menuMainColor))
+
+	const defaultWidth = 90
+
+	// Initialize the list with empty items; items will be set in updateListItems
+	l := list.New([]list.Item{}, itemDelegate{}, defaultWidth, listHeight)
+	l.Title = "Main Menu"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.SetShowTitle(true)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+	l.KeyMap.ShowFullHelp = key.NewBinding() // remove '?' help option
+
+	s := spinner.New()
+	s.Spinner = spinner.Pulse
+
+	m := MenuList{
+		list:       l,
+		header:     header,
+		state:      StateMainMenu,
+		spinner:    s,
+		spinnerMsg: "Action Performing",
+	}
+
+	m.updateListItems()
+
+	m.list.KeyMap.Quit = key.NewBinding(
+		key.WithKeys("esc", "ctrl+c"),
+		key.WithHelp("esc", "quit"),
+	)
+
+	_, err := tea.NewProgram(m, tea.WithAltScreen()).Run()
+	if err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
+
+type item string
+
+func (i item) FilterValue() string { return "" }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
