d80f07d611b385bc17908ae6555baf0d9028930f
Jonathan Munoz <madzumo@gmail.com>
2025-01-01 12:21:20 -0500

    update script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+echo "Hello, DigitalOcean!" > /root/test.txt
 sudo apt-get update
 sudo apt-get install xfce4 xfce4-goodies tightvncserver -y
 sudo apt-get install dbus-x11 -y
 sudo apt-get install expect -y
 curl -fsS https://dl.brave.com/install.sh | sh
-
+sleep 2
 expect -c '
 spawn vncserver
 expect {
@@ -20,9 +20,8 @@ expect {
     "Would you like to enter a view-only password (y/n)?" {
         send "n\r"
     }
-}
-interact'
-
+}'
+sleep 2
 vncserver -kill :1
 echo -e '#!/bin/bash\nxrdb $HOME/.Xresources\nstartxfce4 &\nbrave-browser --no-sandbox --new-window --start-maximized "https://twitch.tv/vortix93"' > ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
